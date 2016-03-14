4fc350a06fcd20e391b77197c828f6b9b288905e
Jorge Castro <keoz@live.com.mx>
2016-03-14 16:31:16 -0600

    adventure works web api

---
+++ Engine/Engine.DataAPI/Models/DTO/ProductCategoryDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Engine.DataAPI.Models.DTO
+{
+    public class ProductCategoryDTO
+    {
+        public int ProductCategoryID { get; set; }
+
+        public int? ParentProductCategoryID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public List<ProductCategory> ProductCategories { get; set; }
+    }
+}
