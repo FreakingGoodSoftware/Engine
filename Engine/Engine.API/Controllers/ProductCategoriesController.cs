f3c231b5e465335a03679f4c54a8fcb3db0b76cd
Jorge Castro <keoz@live.com.mx>
2016-03-16 17:13:49 -0600

    engine data api rest client

---
+++ Engine/Engine.API/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using Engine.API.Filters;
+using Engine.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Engine.API.Controllers
+{
+    [HttpOperationExceptionFilterAttribute]
+    public class ProductCategoriesController : ApiController
+    {
+        private static EngineDataAPI NewEngineDataAPIClient()
+        {
+            var client = new EngineDataAPI(new Uri(""));
+
+            return client;
+        }
+        // GET: api/ProductCategories
+        public async Task<IEnumerable<ProductCategoryDTO>> GetProductCategories()
+        {
+            using (var client = NewEngineDataAPIClient())
+            {
+                var results = await client.ProductCategories.GetProductCategoriesAsync();
+                return results;
+            }
+        }
+
+        // GET: api/ProductCategories/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ProductCategories
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ProductCategories/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ProductCategories/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Engine/Engine.API/Filters/HttpOperationExceptionFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Engine.API.Filters
+{
+    public class HttpOperationExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (context.Exception is Microsoft.Rest.HttpOperationException)
+            {
+                var ex = (Microsoft.Rest.HttpOperationException)context.Exception;
+                context.Response = ex.Response;
+            }
+        }
+    }
+}
