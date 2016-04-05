f00c0278d3860c50c1f7eb34d5ee2f51d49b1bbf
Jorge Castro <keoz@live.com.mx>
2016-04-05 18:32:01 -0500

    menulist

--- Engine/Engine.Core.ViewModels/ViewModels/HubViewModel.cs
+++ Engine/Engine.Core.ViewModels/ViewModels/HubViewModel.cs
@@ -1,4 +1,6 @@
+using Engine.Core.Services;
 using MvvmCross.Core.ViewModels;
+using System.Collections.Generic;
 
 namespace Engine.Core.ViewModels
 {
@@ -10,6 +12,29 @@ namespace Engine.Core.ViewModels
         { 
             get { return _hello; }
             set { SetProperty (ref _hello, value); }
+        }
+
+        public List<string> MenuList { get; set; } = new List<string>
+        {   "Web access",
+            "Navigation",
+            "Forms input",
+            "List presentation",
+            "Services and plugins",
+            "Platform APIs"
+        };
+
+        public IWebAPIService WebAPIService { get; set; }
+
+
+        public HubViewModel(
+            IWebAPIService webAPIService)
+        {
+            //WebAPIService = webAPIService;
+        }
+
+        public void UIStart()
+        {
+            
         }
     }
 }
