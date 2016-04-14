5520e9d699d0558ca5b248cfbd133ab84aa257b1
Jorge Castro <keoz@live.com.mx>
2016-04-14 16:43:32 -0500

    more vms

---
+++ Engine/Engine.Core.ViewModels/ViewModels/FormsInputViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Core.ViewModels.ViewModels
+{
+    public class FormsInputViewModel : MvxViewModel
+    {
+        public List<string> MenuList { get; set; } = new List<string>
+        {   "Numeric input",
+            "Text input",
+            "Data range input",
+            "Date and time input",
+            "Multi-selectors",
+            "Ink"
+        };
+    }
+}
---
+++ Engine/Engine.Core.ViewModels/ViewModels/ListPresentationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Core.ViewModels.ViewModels
+{
+    public class ListPresentationViewModel : MvxViewModel
+    {
+        public List<string> MenuList { get; set; } = new List<string>
+        {   "List view static",
+            "Items and reuse",
+            "Grid layout",
+            "Selection",
+            "Pull to refresh",
+            "Carousel and scrolling"
+        };
+    }
+}
